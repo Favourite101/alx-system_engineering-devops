77e6ac8c95a532a2426b2f81ee5d9a14dd75dfaf
Favour <ofavourite001@gmail.com>
2024-06-03 13:25:30 -0700

    fix3

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-'''get data from API'''
+'''A script that gathers employee name completed
+tasks and total number of tasks from an API
+'''
 
 import re
-import request
+import requests
 import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
             task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
+            emp_name = emp_req.get('name')
             tasks = list(filter(lambda x: x.get('userId') == id, task_req))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print("Employee {} is done with tasks({}/{})".format(emp_name, len(completed_tasks), len(tasks)))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
                     print('\t {}'.format(task.get('title')))
