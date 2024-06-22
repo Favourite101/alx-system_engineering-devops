49e3eb92171a10665f2df1f8698d47b2d646dc5a
Favour <ofavourite001@gmail.com>
2024-06-22 13:00:05 +0100

    ch

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,14 +13,12 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json?allow_over18=true"  # Include NSFW subreddits
-  headers = {"User-Agent": "MyCoolScript (by /u/your_username)"}  # Replace with your username
+    url = f"https://www.reddit.com/r/{subreddit}/about.json?allow_over18=true"
+  headers = {"User-Agent": "Custom"}
   try:
-    response = requests.get(url, headers=headers, allow_redirects=False)  # Don't follow redirects
-    response.raise_for_status()  # Raise exception for non-200 status codes
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    response.raise_for_status()
     data = response.json()
     return data.get("data", {}).get("subscribers", 0)
   except requests.exceptions.RequestException:
-    # Handle any request errors (e.g., network issues, 404)
     return 0
-    
